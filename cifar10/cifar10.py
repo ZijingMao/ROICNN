5f3706c5bcada9a0d7eb7f9991e974d8594932b9
Zijing Mao <mzj168@hotmail.com>
2016-01-30 18:23:42 -0600

    updated new branch ROICNN_0.0.1

--- cifar10/cifar10.py
+++ cifar10/cifar10.py
@@ -34,7 +34,7 @@ FLAGS = tf.app.flags.FLAGS
 # Basic model parameters.
 tf.app.flags.DEFINE_integer('batch_size', 128,
                             """Number of images to process in a batch.""")
-tf.app.flags.DEFINE_string('data_dir', '/tmp/cifar10_data',
+tf.app.flags.DEFINE_string('data_dir', '/home/zijing/tensorflow/data/cifar10_data',
                            """Path to the CIFAR-10 data directory.""")
 
 # Process images of this size. Note that this differs from the original CIFAR
