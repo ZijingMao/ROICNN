f0d71fbfe65ab90fa44a9ed2671aaee18b9e4003
Zijing Mao <mzj168@hotmail.com>
2016-02-11 15:08:28 -0600

    updated the h5py for reading mat file and convert to tensorflow recognized version

---
+++ recorder/convert_to_record.py
@@ -0,0 +1,83 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import tensorflow.python.platform
+
+import numpy as np
+import tensorflow as tf
+import h5py
+
+
+EEG_DATA = '/home/zijing/Downloads/x2rsvp01.mat'
+
+tf.app.flags.DEFINE_string('directory', '/home/zijing/Downloads/',
+                           'Directory to download data files and write the '
+                           'converted result')
+tf.app.flags.DEFINE_integer('validation_size', 1000,
+                            'Number of examples to separate from the training '
+                            'data for the validation set.')
+FLAGS = tf.app.flags.FLAGS
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def convert_to(images, labels, name):
+    num_examples = labels.shape[0]
+    if images.shape[0] != num_examples:
+        raise ValueError("Images size %d does not match label size %d." %
+                         (images.shape[0], num_examples))
+    rows = images.shape[1]
+    cols = images.shape[2]
+    depth = images.shape[3]
+
+    filename = os.path.join(FLAGS.directory, name + '.tfrecords')
+    print('Writing', filename)
+    writer = tf.python_io.TFRecordWriter(filename)
+    for index in range(num_examples):
+        image_raw = images[index].tostring()
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'height': _int64_feature(rows),
+            'width': _int64_feature(cols),
+            'depth': _int64_feature(depth),
+            'label': _int64_feature(int(labels[index])),
+            'image_raw': _bytes_feature(image_raw)}))
+        writer.write(example.SerializeToString())
+
+
+def main(argv):
+    # Get the data.
+    #with h5py.File(EEG_DATA) as f:
+    f = h5py.File(EEG_DATA)
+    train_x = f['train_x'][:]
+    train_y = f['train_y'][:]
+    test_x = f['test_x'][:]
+    test_y = f['test_y'][:]
+
+    # Extract it into numpy arrays.
+    train_images = np.transpose(train_x, [0, 2, 3, 1])
+    train_labels = train_y
+    test_images = np.transpose(test_x, [0, 2, 3, 1])
+    test_labels = test_y
+
+    # Generate a validation set.
+    validation_images = train_images[:FLAGS.validation_size, :, :, :]
+    validation_labels = train_labels[:FLAGS.validation_size]
+    train_images = train_images[FLAGS.validation_size:, :, :, :]
+    train_labels = train_labels[FLAGS.validation_size:]
+
+    # Convert to Examples and write the result to TFRecords.
+    convert_to(train_images, train_labels, 'train')
+    convert_to(validation_images, validation_labels, 'validation')
+    convert_to(test_images, test_labels, 'test')
+
+
+if __name__ == '__main__':
+    tf.app.run()
