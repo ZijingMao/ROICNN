2fc8c0fe64a6120e1d8618b84dd2051efd3c79a1
ZijingMao <mzj168@hotmail.com>
2016-03-09 16:32:16 -0600

    GPU memory not support 64*256*256*1024 feature map

--- rsvp_quick_start/rsvp_roi_cnn_64_chan.py
+++ rsvp_quick_start/rsvp_roi_cnn_64_chan.py
@@ -145,92 +145,95 @@ def do_eval(sess,
 
 def run_training():
     """Train RSVP for a number of steps."""
-    # Get the sets of images and labels for training, validation, and
-    # test on RSVP.
-    data_sets = rsvp_input_data.read_data_sets(EEG_DATA_MAT,
-                                               FLAGS.fake_data,
-                                               reshape_t=False)
-    # Tell TensorFlow that the model will be built into the default Graph.
-    with tf.Graph().as_default():
-        # Generate placeholders for the images and labels.
-        images_placeholder, labels_placeholder, keep_prob = placeholder_inputs(
-            FLAGS.batch_size)
-        # Build a Graph that computes predictions from the inference model.
-        logits = rsvp_quick_cnn_model.inference(images_placeholder,
-                                                keep_prob)
-        # Add to the Graph the Ops for loss calculation.
-        loss = rsvp_quick_cnn_model.loss(logits, labels_placeholder)
-        # Add to the Graph the Ops that calculate and apply gradients.
-        train_op = rsvp_quick_cnn_model.training(loss, FLAGS.learning_rate)
-        # Add the Op to compare the logits to the labels during evaluation.
-        eval_correct = rsvp_quick_cnn_model.evaluation(logits, labels_placeholder)
-        # Build the summary operation based on the TF collection of Summaries.
-        summary_op = tf.merge_all_summaries()
-        # Create a saver for writing training checkpoints.
-        saver = tf.train.Saver()
-        # Create a session for running Ops on the Graph.
-        sess = tf.Session()
-        # Run the Op to initialize the variables.
-        init = tf.initialize_all_variables()
-        sess.run(init)
-        # Instantiate a SummaryWriter to output summaries and the Graph.
-        summary_writer = tf.train.SummaryWriter(FLAGS.train_dir,
-                                                graph_def=sess.graph_def)
-        # And then after everything is built, start the training loop.
-        for step in xrange(FLAGS.max_steps):
-            start_time = time.time()
-            # Fill a feed dictionary with the actual set of images and labels
-            # for this particular training step.
-            feed_dict = fill_feed_dict(data_sets.train,
-                                       0.5,
-                                       images_placeholder,
-                                       labels_placeholder,
-                                       keep_prob)
-            # Run one step of the model.  The return values are the activations
-            # from the `train_op` (which is discarded) and the `loss` Op.  To
-            # inspect the values of your Ops or variables, you may include them
-            # in the list passed to sess.run() and the value tensors will be
-            # returned in the tuple from the call.
-            _, loss_value = sess.run([train_op, loss],
-                                     feed_dict=feed_dict)
-            duration = time.time() - start_time
-            # Write the summaries and print an overview fairly often.
-            if step % 100 == 0:
-                # Print status to stdout.
-                print('Step %d: loss = %.4f (%.3f sec)' % (step, loss_value, duration))
-                # Update the events file.
-                summary_str = sess.run(summary_op, feed_dict=feed_dict)
-                summary_writer.add_summary(summary_str, step)
-            # Save a checkpoint and evaluate the model periodically.
-            if (step + 1) % 200 == 0 or (step + 1) == FLAGS.max_steps:
-                saver.save(sess, FLAGS.train_dir, global_step=step)
-                # Evaluate against the training set.
-                print('Training Data Eval:')
-                do_eval(sess,
-                        eval_correct,
-                        logits,
-                        images_placeholder,
-                        labels_placeholder,
-                        keep_prob,
-                        data_sets.train)
-                # Evaluate against the validation set.
-                print('Validation Data Eval:')
-                do_eval(sess,
-                        eval_correct,
-                        logits,
-                        images_placeholder,
-                        labels_placeholder,
-                        keep_prob,
-                        data_sets.validation)
-                # Evaluate against the test set.
-                print('Test Data Eval:')
-                do_eval(sess,
-                        eval_correct,
-                        logits,
-                        images_placeholder,
-                        labels_placeholder,
-                        keep_prob,
-                        data_sets.test)
+    with tf.Graph().as_default(), tf.device('/cpu:0'):
+        # Get the sets of images and labels for training, validation, and
+        # test on RSVP.
+        data_sets = rsvp_input_data.read_data_sets(EEG_DATA_MAT,
+                                                   FLAGS.fake_data,
+                                                   reshape_t=False)
+        # Tell TensorFlow that the model will be built into the default Graph.
+        with tf.Graph().as_default():
+            # Generate placeholders for the images and labels.
+            images_placeholder, labels_placeholder, keep_prob = placeholder_inputs(
+                FLAGS.batch_size)
+            # Build a Graph that computes predictions from the inference model.
+            logits = rsvp_quick_cnn_model.inference(images_placeholder,
+                                                    keep_prob)
+            # Add to the Graph the Ops for loss calculation.
+            loss = rsvp_quick_cnn_model.loss(logits, labels_placeholder)
+            # Add to the Graph the Ops that calculate and apply gradients.
+            train_op = rsvp_quick_cnn_model.training(loss, FLAGS.learning_rate)
+            # Add the Op to compare the logits to the labels during evaluation.
+            eval_correct = rsvp_quick_cnn_model.evaluation(logits, labels_placeholder)
+            # Build the summary operation based on the TF collection of Summaries.
+            summary_op = tf.merge_all_summaries()
+            # Create a saver for writing training checkpoints.
+            saver = tf.train.Saver()
+            # Create a session for running Ops on the Graph.
+            sess = tf.Session()
+            # Run the Op to initialize the variables.
+            init = tf.initialize_all_variables()
+            sess.run(init)
+            # Instantiate a SummaryWriter to output summaries and the Graph.
+            summary_writer = tf.train.SummaryWriter(FLAGS.train_dir,
+                                                    graph_def=sess.graph_def)
+            # And then after everything is built, start the training loop.
+            for i in xrange(2):
+                with tf.device('/gpu:%d' % i):
+                    for step in xrange(FLAGS.max_steps):
+                        start_time = time.time()
+                        # Fill a feed dictionary with the actual set of images and labels
+                        # for this particular training step.
+                        feed_dict = fill_feed_dict(data_sets.train,
+                                                   0.5,
+                                                   images_placeholder,
+                                                   labels_placeholder,
+                                                   keep_prob)
+                        # Run one step of the model.  The return values are the activations
+                        # from the `train_op` (which is discarded) and the `loss` Op.  To
+                        # inspect the values of your Ops or variables, you may include them
+                        # in the list passed to sess.run() and the value tensors will be
+                        # returned in the tuple from the call.
+                        _, loss_value = sess.run([train_op, loss],
+                                                 feed_dict=feed_dict)
+                        duration = time.time() - start_time
+                        # Write the summaries and print an overview fairly often.
+                        if step % 100 == 0:
+                            # Print status to stdout.
+                            print('Step %d: loss = %.4f (%.3f sec)' % (step, loss_value, duration))
+                            # Update the events file.
+                            summary_str = sess.run(summary_op, feed_dict=feed_dict)
+                            summary_writer.add_summary(summary_str, step)
+                        # Save a checkpoint and evaluate the model periodically.
+                        if (step + 1) % 200 == 0 or (step + 1) == FLAGS.max_steps:
+                            saver.save(sess, FLAGS.train_dir, global_step=step)
+                            # Evaluate against the training set.
+                            print('Training Data Eval:')
+                            do_eval(sess,
+                                    eval_correct,
+                                    logits,
+                                    images_placeholder,
+                                    labels_placeholder,
+                                    keep_prob,
+                                    data_sets.train)
+                            # Evaluate against the validation set.
+                            print('Validation Data Eval:')
+                            do_eval(sess,
+                                    eval_correct,
+                                    logits,
+                                    images_placeholder,
+                                    labels_placeholder,
+                                    keep_prob,
+                                    data_sets.validation)
+                            # Evaluate against the test set.
+                            print('Test Data Eval:')
+                            do_eval(sess,
+                                    eval_correct,
+                                    logits,
+                                    images_placeholder,
+                                    labels_placeholder,
+                                    keep_prob,
+                                    data_sets.test)
 
 
 def main(_):
