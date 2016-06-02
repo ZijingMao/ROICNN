977dbbd42e9acbdddc70f1415620c8e10f3f7d48
root <chenjx_sust@foxmail.com>
2016-06-02 11:28:54 -0500

    no relu brance

--- rsvp_quick_start/rsvp_quick_inference.py
+++ rsvp_quick_start/rsvp_quick_inference.py
@@ -135,12 +135,11 @@ def inference_augment_s_filter(images):
     return augment
 
 
-def inference_pooling_l2norm_filter(images, kwidth=5):
+def inference_pooling_l2norm_filter(images, kwidth=2):
     # channel domain pooling mapper
     kheight=2
     split_dim = 1   # 1 represents split on spatial domain
-    input_image_list = split_eeg.split_eeg_signal_axes(images,
-                                                       split_dim=split_dim)
+    input_image_list = split_eeg.split_eeg_signal_axes(images, split_dim=split_dim)
     input_image_length = len(input_image_list)
 
     # the pooling mapper should choose half size of the image size
@@ -160,6 +159,33 @@ def inference_pooling_l2norm_filter(images, kwidth=5):
 
     _print_tensor_size(pool_s)
     return pool_s
+
+
+# def inference_pooling_l2norm_filter(images, kwidth=5):
+#     # channel domain pooling mapper
+#     kheight=2
+#     split_dim = 1   # 1 represents split on spatial domain
+#     input_image_list = split_eeg.split_eeg_signal_axes(images,
+#                                                        split_dim=split_dim)
+#     input_image_length = len(input_image_list)
+#
+#     # the pooling mapper should choose half size of the image size
+#     pool_s, _ = concat_eeg.pool_eeg_signal_channel(input_image_list, input_image_length/2, 1)
+#     _print_tensor_size(pool_s)
+#
+#     pool_s = tf.mul(pool_s,pool_s)
+#     pool_s = tf.mul(float(kwidth), pool_s)
+#
+#     pool_s = tf.nn.avg_pool(pool_s, ksize=[1, 1, kwidth, 1],
+#                             strides=[1, 1, kwidth, 1], padding='VALID')
+#
+#     pool_s = tf.sqrt(pool_s)
+#
+#     pool_s = tf.nn.max_pool(pool_s, ksize=[1, kheight, 1, 1],
+#                              strides=[1, kheight, 1, 1], padding='VALID')
+#
+#     _print_tensor_size(pool_s)
+#     return pool_s
 
 
 def inference_pooling_s_filter(images, kheight=2, kwidth=2):
@@ -303,7 +329,7 @@ def inference_local_st5_filter(images, conv_layer_scope, in_feat=1, out_feat=4):
         conv = tf.nn.conv2d(augment, kernel, [1, 5, 5, 1], padding='SAME')
         biases = _variable_on_cpu('biases', [out_feat], tf.constant_initializer(0.0))
         conv_output = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape().as_list())
-        conv_output = tf.nn.relu(bias, name=scope.name)
+        conv_output = tf.nn.relu(biases, name=scope.name)
         # conv_output = tf.nn.dropout(conv_output, 0.5)
         # conv_output = tf.nn.local_response_normalization(conv_output, depth_radius=5, alpha=0.0001)
         _print_tensor_size(conv_output)
